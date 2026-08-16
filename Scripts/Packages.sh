c839e15a0619a84dbd3d59c39293639875fea316
bro123pan <peng-k@163.com>
2026-08-16 20:35:32 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -63,6 +63,10 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 
 #UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
+UPDATE_PACKAGE "unishare" "kenzok8/jell" "main" "pkg"
+UPDATE_PACKAGE "webdav2" "kenzok8/jell" "main" "pkg"
+UPDATE_PACKAGE "luci-app-unishare" "kenzok8/jell" "main" "pkg"
+
 #UPDATE_PACKAGE "athena-led" "unraveloop/JDC-AX6600-Athena-LED-Controller" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 #UPDATE_PACKAGE "diskman" "sbwml/luci-app-diskman" "main"
